d9cb8064f48e50ebb985b580e7a21ba6cb1963b1
Stephan-MDD <stephan.mdd@gmail.com>
2020-09-24 15:05:55 +0200

    Updates

---
+++ precache-manifest.fd6d8a68ae15fd7661dfd5d137b68796.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d08d5fadb9a9ac95a357609729a74cc",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "b96a148abf4219d010b7",
+    "url": "/website/static/css/main.edcb3491.chunk.css"
+  },
+  {
+    "revision": "c9473481fe1808dae61b",
+    "url": "/website/static/js/2.3fbe5d21.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/website/static/js/2.3fbe5d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96a148abf4219d010b7",
+    "url": "/website/static/js/main.0d51025b.chunk.js"
+  },
+  {
+    "revision": "9ff52643d7a5c9f13897",
+    "url": "/website/static/js/runtime-main.0e84ac12.js"
+  },
+  {
+    "revision": "644575c1c9f7b4f2b4b14b09a52cebd2",
+    "url": "/website/static/media/img-template.644575c1.png"
+  },
+  {
+    "revision": "ebd30485f33b26a58b21f4da0fc7524d",
+    "url": "/website/static/media/profile.ebd30485.png"
+  }
+]);
